8c1e662ebc95630e68f0562d1e3240e638885943
Xianda Yu <176912231@163.com>
2015-05-15 00:19:54 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding:utf8
+
+from simfish import application, route, SimpleTemplate
+
+@route('/index.html')
+def index(request):
+	return SimpleTemplate(filename="index.html").render()
+
+@route('/')
+def hello(request):
+    return "hello world"
+
+
+app = application(port=8086)
+app.run()
