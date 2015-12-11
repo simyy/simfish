82161c5893e5cfa649ddfa100d3c72cb85eb8064
yxd123 <176912231@163.com>
2015-12-12 00:42:35 +0800

    update some functions

--- test.py
+++ test.py
@@ -3,9 +3,11 @@
 
 from simfish import application, route, SimpleTemplate
 
+
 @route('/index.html')
 def index(request):
 	return SimpleTemplate(filename="index.html").render()
+
 
 @route('/')
 def hello(request):
